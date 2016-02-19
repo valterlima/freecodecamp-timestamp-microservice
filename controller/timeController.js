0ba250418f8f00315807b77fcb2d72660bc8bfcc
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-19 10:32:34 -0300

    Project done!

---
+++ controller/timeController.js
@@ -0,0 +1,21 @@
+var moment = require('moment');
+
+exports.parse = function(param){
+	param = (isNaN(param)) ? param : parseInt(param);
+	
+	if (typeof param === 'number'){
+		var unix = param;
+		var natural = moment.unix(param).format("MMMM D, YYYY");
+	}
+	else if(typeof param === 'string'){
+		var unix = moment(param, "MMMM D, YYYY").unix('x');
+		var natural = param;
+	}
+	
+	var response = {
+		"unix": unix,
+		"natural": natural
+	}
+
+	return response;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var timeController = require('../controller/timeController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log('here', req.body);
   res.render('index', { title: 'Express' });
+});
+
+router.get('/:param', function(req, res, next) {
+	var param = req.params.param;	
+	res.send(timeController.parse(param));
 });
 
 module.exports = router;
